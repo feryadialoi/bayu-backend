9af349acb4ab028bfea105970d358fdfda55af0e
Feryadi Aloi <feryadialoi@gmail.com>
2021-03-14 00:28:55 +0700

    add project to remote repo

---
+++ src/main/java/dev/feryadi/backend/bayu/BayuApplication.java
@@ -0,0 +1,13 @@
+package dev.feryadi.backend.bayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/feryadi/backend/bayu/BayuApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.feryadi.backend.bayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
