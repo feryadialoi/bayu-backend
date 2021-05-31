e389297ea6dd37e453fb54de8d8566d49e98ee50
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-01 01:16:42 +0700

    add builder to user response

--- src/main/java/dev/feryadi/backend/bayu/model/response/UserResponse.java
+++ src/main/java/dev/feryadi/backend/bayu/model/response/UserResponse.java
@@ -1,12 +1,14 @@
 package dev.feryadi.backend.bayu.model.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class UserResponse {
     private Long id;
     private String name;
