681924b7732d1c0ea99af43ad154e7ed3450f32d
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-07 06:24:29 +0700

    update to pageable in ListUserMutationRequest

--- src/main/java/dev/feryadi/backend/bayu/model/request/ListUserMutationRequest.java
+++ src/main/java/dev/feryadi/backend/bayu/model/request/ListUserMutationRequest.java
@@ -4,14 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ListUserMutationRequest {
-    private Integer page;
-    private Integer size;
+    private Pageable pageable;
     private String startDate;
     private String endDate;
 }
