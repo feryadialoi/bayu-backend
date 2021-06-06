c5779ff1a995263f0daa6ac2ac7f9b89d5a33187
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-07 06:26:22 +0700

    use pageable in UserTransactionController

--- src/main/java/dev/feryadi/backend/bayu/controller/UserTransactionController.java
+++ src/main/java/dev/feryadi/backend/bayu/controller/UserTransactionController.java
@@ -1,18 +1,16 @@
 package dev.feryadi.backend.bayu.controller;
 
-import dev.feryadi.backend.bayu.exception.NotFoundException;
 import dev.feryadi.backend.bayu.model.request.ListUserTransactionRequest;
 import dev.feryadi.backend.bayu.model.response.ApiResponse;
 import dev.feryadi.backend.bayu.model.response.UserTransactionResponse;
 import dev.feryadi.backend.bayu.service.UserTransactionService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,13 +25,11 @@ public class UserTransactionController extends BaseController {
     @PreAuthorize("hasRole('ADMIN') or @userSecurity.hasUserId(authentication, #userId)")
     @GetMapping(value = {"/api/v1/users/{userId}/transactions"})
     public ResponseEntity<ApiResponse<List<UserTransactionResponse>>> getUserTransactions(
-            @PathVariable(value = "userId") Long userId,
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "10") Integer size
+            Pageable pageable,
+            @PathVariable(value = "userId") Long userId
     ) {
         ListUserTransactionRequest listUserTransactionRequest = ListUserTransactionRequest.builder()
-                .page(page)
-                .size(size)
+                .pageable(pageable)
                 .build();
 
         return createResponse(
