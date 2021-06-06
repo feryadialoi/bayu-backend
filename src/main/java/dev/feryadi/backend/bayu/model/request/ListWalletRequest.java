75195572d301e418b2708eebbed04f66b1299e82
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-07 06:25:08 +0700

    update to pageable in ListWalletRequest

--- src/main/java/dev/feryadi/backend/bayu/model/request/ListWalletRequest.java
+++ src/main/java/dev/feryadi/backend/bayu/model/request/ListWalletRequest.java
@@ -4,12 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Pageable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ListWalletRequest {
-    private Integer size;
-    private Integer page;
+    private Pageable pageable;
 }
