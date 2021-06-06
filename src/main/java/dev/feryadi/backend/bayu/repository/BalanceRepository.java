a455c167a31bbd41ffca75d503ab4547bfa7b123
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-07 06:15:22 +0700

    BalanceRepository extends jpa specification executor

--- src/main/java/dev/feryadi/backend/bayu/repository/BalanceRepository.java
+++ src/main/java/dev/feryadi/backend/bayu/repository/BalanceRepository.java
@@ -2,6 +2,7 @@ package dev.feryadi.backend.bayu.repository;
 
 import dev.feryadi.backend.bayu.entity.Balance;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BalanceRepository extends JpaRepository<Balance, Long> {
+public interface BalanceRepository extends JpaRepository<Balance, Long>, JpaSpecificationExecutor<Balance> {
 }
