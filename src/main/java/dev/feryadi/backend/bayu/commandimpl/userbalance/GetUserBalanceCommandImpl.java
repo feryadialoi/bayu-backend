170c815da9223070b96dfd9ba7e59ce26f914a72
Feryadi Aloi <feryadialoi@gmail.com>
2021-06-07 06:18:59 +0700

    clean up GetUserBalanceCommandImpl

--- src/main/java/dev/feryadi/backend/bayu/commandimpl/userbalance/GetUserBalanceCommandImpl.java
+++ src/main/java/dev/feryadi/backend/bayu/commandimpl/userbalance/GetUserBalanceCommandImpl.java
@@ -2,15 +2,17 @@ package dev.feryadi.backend.bayu.commandimpl.userbalance;
 
 import dev.feryadi.backend.bayu.command.userbalance.GetUserBalanceCommand;
 import dev.feryadi.backend.bayu.entity.User;
-import dev.feryadi.backend.bayu.entity.Wallet;
-import dev.feryadi.backend.bayu.exception.NotFoundException;
 import dev.feryadi.backend.bayu.exception.UserNotFoundException;
+import dev.feryadi.backend.bayu.exception.WalletNotFoundException;
 import dev.feryadi.backend.bayu.model.request.command.GetUserBalanceCommandRequest;
 import dev.feryadi.backend.bayu.model.response.BalanceResponse;
 import dev.feryadi.backend.bayu.modelmapper.BalanceMapper;
+import dev.feryadi.backend.bayu.repository.BalanceRepository;
 import dev.feryadi.backend.bayu.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 
 @Component
@@ -18,6 +20,7 @@ import org.springframework.stereotype.Component;
 public class GetUserBalanceCommandImpl implements GetUserBalanceCommand {
 
     private final UserRepository userRepository;
+    private final BalanceRepository balanceRepository;
     private final BalanceMapper balanceMapper;
 
 
@@ -25,24 +28,18 @@ public class GetUserBalanceCommandImpl implements GetUserBalanceCommand {
     public BalanceResponse execute(GetUserBalanceCommandRequest request) {
         Long userId = request.getUserId();
 
-        return userRepository.findById(userId)
-                .map(User::getWallet)
-                .map(Wallet::getBalance)
-                .map(balanceMapper::mapBalanceToBalanceResponse)
-                .orElseThrow(() -> new UserNotFoundException("User with id " + userId + " doesn't have wallet"));
+        Optional<User> optionalUser = userRepository.findById(userId);
 
-//        Optional<User> optionalUser = userRepository.findById(userId);
-//
-//        if (optionalUser.isPresent()) {
-//            User user = optionalUser.get();
-//            if (user.getWallet() != null) {
-//                return balanceMapper.mapBalanceToBalanceResponse(user.getWallet().getBalance());
-//            }
-//
-//            throw new NotFoundException("User with id " + userId + " doesn't have wallet");
-//        }
-//
-//        throw new NotFoundException("User with id " + userId + " not found");
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getWallet() != null) {
+                return balanceMapper.mapBalanceToBalanceResponse(user.getWallet().getBalance());
+            }
+
+            throw new WalletNotFoundException("User with id " + userId + " doesn't have wallet");
+        }
+
+        throw new UserNotFoundException("User with id " + userId + " not found");
 
     }
 }
